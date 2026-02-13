b0d7b4bb43e1b43a288f5e85f9bce9b0f0a66701
Fritz Duchardt <fritz@duchardt.net>
2026-02-13 11:06:51 +0100

    fix: improve news description

--- streaming_mcp_server.py
+++ streaming_mcp_server.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+import logging
 
-import os
 from mcp.server.fastmcp import FastMCP
 from social.ts import trump as t
 from finance.twelvedata import historical_data as hd
 from news.rapid import news_search
 
-# Initialize the MCP server
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 mcp = FastMCP(name="StatelessServer",stateless_http=False, host="0.0.0.0")
+
 
 @mcp.tool()
 def trump_tweets(start_date: str, end_date: str) -> str:
@@ -40,7 +46,7 @@ def world_news(query: str, limit: int, country: str, time_published: str) -> str
         query: specific query about what happened
         limit: int of number of news items
         country: country code, e.g. US, UK, GE
-        time_published: duration to the past from now, e.g. 1h, 1d, 1w
+        time_published: duration to the past from now in minutes, hours, days, or years, e.g. 1m, 1h, 1d, 1y.
     """
     # Your existing logic here
     data = news_search.claw(query, limit, country, time_published)
